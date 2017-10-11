42c768685b5fb4eb805656da05682aec67ec02a2
Elo <wanang.elodie@hotmail.fr>
2017-10-11 12:53:43 +0200

    Single button adding a marker trial

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
     lat: 3.3192925,
     lng: 4.4218547,
   };
+  const ng = {
+    lat: 8.9233587,
+    lng: -0.3674603,
+  };
 
 const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
@@ -14,4 +18,21 @@ const map = new google.maps.Map(document.getElementById('map'), {
            position: afri,
            map: map
          });
+
+         function addMarker(location, map) {
+           // Add the marker at the clicked location, and add the next-available label
+           // from the array of alphabetical characters.
+           var marker = new google.maps.Marker({
+             position: location,
+             label: labels[labelIndex++ % labels.length],
+             map: map
+           });
+         }
+         $('#Nigeriabutton').event.addListener(map, 'click', function(event) {
+             addMarker(ng, map);
+           });
+
+
+
+
 });
