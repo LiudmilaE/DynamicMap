a01a5bb8ec9375cce15b451c02a241830c1ba7a2
Liudmyla Iefremova <liudmyla.iefremova@gmail.com>
2017-10-11 15:19:53 +0200

    add first org in seeds and fixed marker

--- bin/seeds.js
+++ bin/seeds.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 const User = require('../models/user');
+const Organization = require('../models/organization');
 
 mongoose.connect("mongodb://localhost/dynamic-map");
 const salt = bcrypt.genSaltSync(bcryptSalt);
@@ -24,6 +25,25 @@ const admin2 = new User({
   isAdmin: true,
 });
 
+const incubator1 = new Organization({
+  name: "Entrepreneur Incubator",
+  description: "Business Development Service. For more information - check up web site ei.co.za",
+  contacts: {
+    email: "ayanda@em-solutions.co.za",
+    phone: "218392281",
+    },
+  address: {
+    zip: "7925",
+    country: "South Africa",
+    city: "Cape Town",
+    street: "Unit 6C, North Block, 31 Brickfield Road, Woodstock, Salt River",
+    },
+  category: 'incubator',
+  status: 'accepted',
+  ownerId: "59de0ff3969e5e08382e0de9",
+  location: { type: "Point", coordinates: [ 18.4551591, -33.9353179] }
+});
+
 User.create(admin1, (err, user) => {
   if (err) {
 	throw err;
@@ -39,3 +59,11 @@ User.create(admin2, (err, user) => {
   console.log(user);
   mongoose.connection.close();
 });
+
+Organization.create(incubator1, (err, organization) => {
+  if (err) {
+  throw err;
+  }
+  console.log(organization);
+  mongoose.connection.close();
+})
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const Organization = require('../models/organization');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DynamicMap' });
+	Organization.find((error, organizations) => {
+		if (error) { next(error); }
+    else {
+      res.render('index', { organizations });
+    }
+	})
+  //res.render('index', { title: 'DynamicMap' });
 });
 
 module.exports = router;
